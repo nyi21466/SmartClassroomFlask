0030a7222884988c1ea12dcae9c5dae38c34b079
Nyi Nyi <62074560+nyi21466@users.noreply.github.com>
2025-11-22 23:22:34 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,24 +1,46 @@
 from flask import Flask
 from flask import render_template, request
-import mysql.connector
-mydb = mysql.connector.connect(
-  host="eishwesin.mysql.pythonanywhere-services.com",
-  user="eishwesin",
-  passwd="thesisproject",
-  database="eishwesin$studentdb"
-)
+import sqlite3
+import random
+import datetime
+
+mydb = sqlite3.connect('Makersdatabase.db', check_same_thread=False)
+mycursor = mydb.cursor()
+
+mycursor.execute('''
+    CREATE TABLE IF NOT EXISTS studentrecordnew (
+        Id INT,
+        time TIMESTAMP
+    )
+''')
+
+idlist = [1,2,3,4,5]
+
+# for i in range(30):
+#     currentDateTime = datetime.datetime.now()
+#     mycursor.execute("INSERT INTO studentrecordnew (Id, time) VALUES (?, ?)", (int(random.choice(idlist)), currentDateTime.strftime("%Y-%m-%d %H:%M:%S")))
+#mydb.commit()
+
 app = Flask(__name__)
 @app.route("/")
 def index():
+    # get the current datetime and store it in a variable
+    currentDateTime = datetime.datetime.now()
+    mycursor.execute("INSERT INTO studentrecordnew (Id, time) VALUES (?, ?)", (int(random.choice(idlist)), currentDateTime.strftime("%Y-%m-%d %H:%M:%S")))
+    mydb.commit()
     return render_template('login.html')
 @app.route("/change", methods=['POST'])
 def change():
-     if request.method == 'POST':
+    # get the current datetime and store it in a variable
+    currentDateTime = datetime.datetime.now()
+    mycursor.execute("INSERT INTO studentrecordnew (Id, time) VALUES (?, ?)", (int(random.choice(idlist)), currentDateTime.strftime("%Y-%m-%d %H:%M:%S")))
+    mydb.commit()
+    if request.method == 'POST':
        name = request.form['name']
        pw = request.form['pw']
        print(name + pw)
        if name == "admin" and pw == "12345":
-	       return render_template('show.html')
+           return render_template('show.html')
        else:
            return render_template('login.html')
 
@@ -29,31 +51,32 @@ def showrecord():
        print(studentid)
        if studentid == "1":
            mycursor = mydb.cursor()
-           mycursor.execute("SELECT * FROM studentrecordnew WHERE id=1")
+           mycursor.execute("SELECT * FROM studentrecordnew WHERE Id=1")
            records = mycursor.fetchall()
            return render_template('show.html',idnumber="Student id 1:",records=records)
        elif studentid == "2":
            mycursor = mydb.cursor()
-           mycursor.execute("SELECT * FROM studentrecordnew WHERE id=2")
+           mycursor.execute("SELECT * FROM studentrecordnew WHERE Id=2")
            records = mycursor.fetchall()
            return render_template('show.html',idnumber="Student id 2:",records=records)
        elif studentid == "3":
            mycursor = mydb.cursor()
-           mycursor.execute("SELECT * FROM studentrecordnew WHERE id=3")
+           mycursor.execute("SELECT * FROM studentrecordnew WHERE Id=3")
            records = mycursor.fetchall()
            return render_template('show.html',idnumber="Student id 3:",records=records)
        elif studentid == "4":
            mycursor = mydb.cursor()
-           mycursor.execute("SELECT * FROM studentrecordnew WHERE id=4")
+           mycursor.execute("SELECT * FROM studentrecordnew WHERE Id=4")
            records = mycursor.fetchall()
            return render_template('show.html',idnumber="Student id 4:",records=records)
        elif studentid == "5":
            mycursor = mydb.cursor()
-           mycursor.execute("SELECT * FROM studentrecordnew WHERE id=5")
+           mycursor.execute("SELECT * FROM studentrecordnew WHERE Id=5")
            records = mycursor.fetchall()
            return render_template('show.html',idnumber="Student id 5:",records=records)
        else:
            return render_template('show.html')
 
 if __name__ == "__main__":
-    app.run(host='172.20.10.7', port=8050)
+    app.run(host='172.20.10.4', port=8050)
+    #app.run(host="0.0.0.0", port=5001, debug=True)
